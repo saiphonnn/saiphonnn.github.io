82c135098c8d972edaa4672291dbe9e5b38cc4c1
saiphonnn <125313622+saiphonnn@users.noreply.github.com>
2023-03-03 01:32:03 -0500

    Update modified.php

--- modified.php
+++ modified.php
@@ -1,29 +1,5 @@
 <?php    
     
-include "connection.php";    
-    
-if(isset($_GET['id'])){    
-$sql = "delete from shifttimes where id = '".$_GET['id']."'";    
-$result = mysql_query($sql);    
-}    
-    
-$sql = "select * from shifttimes";    
-$result = mysql_query($sql);    
-?>    
-<html>    
-    <body>    
-        <table width = "100%" border = "1" cellspacing = "1" cellpadding = "1">    
-            <tr>    
-                <td>Id</td>    
-                <td>Check-In</td>    
-                <td>Check-Out</td>      
-            </tr>    
-        </table>    
-    </body>    
-</html>
-
-<?php    
-    
 while($row = mysql_fetch_object($result)){    
     
     
