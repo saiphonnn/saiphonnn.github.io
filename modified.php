163d5050fa342d3f7e05f9c3447cfbae0f998476
saiphonnn <125313622+saiphonnn@users.noreply.github.com>
2023-03-03 01:20:19 -0500

    Update modified.php

--- modified.php
+++ modified.php
@@ -37,7 +37,8 @@ while($row = mysql_fetch_object($result)){
         </td>  
         <td>  
             <?php echo $row->checkOut;?>  
-        </td>      
+        </td>   
+        <td>
             <?php echo $row->id;?>" onclick="return confirm('Are You Sure')">Delete    
         </a> | <a href="index.php?id =     
             <?php echo $row->id;?>" onclick="return confirm('Are You Sure')">Edit    
